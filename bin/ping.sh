2668429c1bd892dd0b7ac3c4e6f8c42c20059c8d
pmnordkvist <pm.nordkvist@gmail.com>
2013-06-23 09:52:40 +0300

    More dynamic paths. Less hard code

--- bin/ping.sh
+++ bin/ping.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 LOGFILE=/var/log/ping.log 
+BACKUPPATH=/backupbox
 
 while ! test -f /tmp/stop-my-script
 do
-  /backupbox/bin/gatherData.sh
-	curl -s -X POST -d @/tmp/ping.txt http://ping.backupbox.se/ping.php > /dev/null
+	$BACKUPPATH/bin/gatherData.sh $BACKUPPATH
+	curl -s -X POST -d @/tmp/ping.txt http://ping.backupbox.se/ping.php  > /dev/null
 	sleep 60
 done
