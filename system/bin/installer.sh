7993a0ee3718223ccd83eed217fe17d58ecefa15
Henrik Hedelund <henke.hedelund@gmail.com>
2013-10-15 19:09:10 +0200

    [installer] installer and chef adaptaions to new directory structure

--- system/bin/installer.sh
+++ system/bin/installer.sh
@@ -6,19 +6,31 @@
 # A modern Debian based GNU/Linux distribution is assumed.
 #
 
-if [ $EUID -ne 0 ]; then echo "Please run as root" 1>&2; exit 1; fi
-
 printc()  { echo -ne "\e[$2m$1\e[0m"; }
 notice()  { printc "$1" "1;34"; }
 success() { printc "$1" "0;32"; }
 error()   { printc "$1" "0;31"; }
 warning() { printc "$1" "1;33"; }
 
-cd "$(dirname "$0")"
-CWD="$(pwd)"
-BOX_DIR="$(cd .. && pwd)"
-CONFIG="$CWD/chef/config/solo.rb"
-JSON="$CWD/chef/config/node.json"
+if [ $EUID -ne 0 ]; then notice "Please run as root\n" 1>&2; exit 1; fi
+
+cd "$(dirname ""$0"")"
+source ../config/dirs.cfg
+
+(test -n "$ROOT_DIR" \
+    && test -n "$BIN_DIR" \
+    && test -n "$LIB_DIR" \
+    && test -n "$WWW_DIR" \
+    && test -n "$DATA_DIR" \
+    && test -n "$CACHE_DIR" \
+    && test -n "$LOG_DIR" \
+    && test -n "$CONFIG_DIR" \
+        ) || {
+        error "The config doesn't define required variables\n" 1>&2; exit 2;
+    }
+
+CONFIG="$CONFIG_DIR/chef/config/solo.rb"
+JSON="$CONFIG_DIR/chef/config/node.json"
 LOGLEVEL="warn" # debug, info, warn, error, fatal
 CHEF="$(which chef-solo)"
 
@@ -32,13 +44,27 @@ then
 fi
 
 notice "Running setup recipes..\n"
-export BOX_DIR
+
+# export vars required by the box recipe
+export ROOT_DIR
+export BIN_DIR
+export LIB_DIR
+export WWW_DIR
+export DATA_DIR
+export CACHE_DIR
+export LOG_DIR
+
 "$CHEF" \
     --config "$CONFIG" \
     --json-attributes "$JSON" \
     --log_level "$LOGLEVEL" \
     --color
 
-notice "Installation complete!\n"
-exit 0
+if [ $? -eq 0 ]
+then
+    success "Installation complete!\n"
+else
+    error "Installation failed to complete.\n" 1>&2; exit 3;
+fi
 
+exit 0
--- system/config/chef/cookbooks/box/attributes/default.rb
+++ system/config/chef/cookbooks/box/attributes/default.rb
@@ -1,3 +1,10 @@
+set[:dirs][:system][:root] = ENV['ROOT_DIR'] || nil
+set[:dirs][:system][:bin] = ENV['BIN_DIR'] || nil
+set[:dirs][:system][:lib] = ENV['LIB_DIR'] || nil
+set[:dirs][:system][:www] = ENV['WWW_DIR'] || nil
+set[:dirs][:data][:data] = ENV['DATA_DIR'] || nil
+set[:dirs][:data][:cache] = ENV['CACHE_DIR'] || nil
+set[:dirs][:data][:log] = ENV['LOG_DIR'] || nil
 set[:pmount][:package] = 'pmount'
 set[:ifuse][:package] = 'ifuse'
 set[:idevicepair][:package] = 'libimobiledevice-utils'
