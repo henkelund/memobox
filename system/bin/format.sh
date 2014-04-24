94a41c0dd7eb9e1f6855220f41b8f8d258b09a02
root <root@ubtu>
2014-04-24 21:22:33 +0000

    Fixed bug in format script

--- system/bin/format.sh
+++ system/bin/format.sh
@@ -3,7 +3,7 @@
 printf "n\np\n1\n\n\nw\n" | sudo fdisk /dev/sda
 mkfs.ext3 /dev/sda1
 mount /dev/sda1 /HDD
-/etc/init.d/uwsgi restart
-/etc/init.d/nginx restart
 chown www-data:www-data /HDD
 mkdir /HDD/log
+/etc/init.d/uwsgi restart
+/etc/init.d/nginx restart
