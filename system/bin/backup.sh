73996d5dcd27b525de6f26df5358439d16e559d0
root <root@ubtu>
2014-06-01 15:10:31 +0000

    Moved BOXUSER config to local file in data folder

--- system/bin/backup.sh
+++ system/bin/backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SYSTEM_DIR="$(dirname "$BIN_DIR")"
+BACKUP_DIR="$(dirname "$SYSTEM_DIR")/data"
+source "$BACKUP_DIR/local.cfg"
+
 # Create backup of main database
 rm /HDD/tmp.db
 /usr/bin/sqlite3 /HDD/index.db <<EOF
@@ -8,12 +13,12 @@ rm /HDD/tmp.db
 EOF
 
 # Backup all content
-rdiff-backup --force --exclude /HDD/index.db /HDD/ root@nordkvist.backupbox.se::/HDD/
+rdiff-backup --force --exclude /HDD/index.db /HDD/ root@$BOXUSER.backupbox.se::/HDD/
 
 # Copy database
-ssh nordkvist.backupbox.se 'cp /HDD/tmp.db /HDD/index.db && chmod 777 /HDD/index.db'
+ssh $BOXUSER.backupbox.se 'cp /HDD/tmp.db /HDD/index.db && chmod 777 /HDD/index.db'
 
 # Restart uwsgi on remote server in order to reload database
-ssh nordkvist.backupbox.se '/etc/init.d/uwsgi restart'
+ssh $BOXUSER.backupbox.se '/etc/init.d/uwsgi restart'
 
 #done
