a358cbfb06dab23f2c15adc13623a649328a4024
root <root@ubtu>
2014-06-09 22:03:08 +0000

    rsync without prompting for password

--- system/bin/backup.sh
+++ system/bin/backup.sh
@@ -8,6 +8,7 @@ echo "$BACKUP_DIR/local.cfg"
 echo "$BOXUSER"
 
 # Create backup of main database
+echo "BackupDB"
 rm /HDD/tmp.db
 /usr/bin/sqlite3 /HDD/index.db <<EOF
 .timeout 20000
@@ -15,10 +16,20 @@ rm /HDD/tmp.db
 EOF
 
 # Backup all content
-rdiff-backup --force --exclude /HDD/index.db /HDD/ root@$BOXUSER.backupbox.se::/backups/$BOXUSER
+#rdiff-backup --force --exclude /HDD/index.db /HDD/ root@$BOXUSER.backupbox.se::/backups/$BOXUSER
+# First copy config file and thumbnails
+echo "Backup config file"
+rsync -avz -e ssh --progress --exclude index.db /HDD/local.cfg root@$BOXUSER.backupbox.se:/backups/$BOXUSER
+#rsync -avz --progress --exclude index.db /HDD/cache root@$BOXUSER.backupbox.se:/backups/$BOXUSER/cache
 
-# Copy database
-ssh $BOXUSER.backupbox.se 'cp /backups/$BOXUSER/tmp.db /backups/$BOXUSER/index.db && chmod 777 /backups/$BOXUSER/index.db && chown www-data:www-data /backups/$BOXUSER/index.db'
+# Second, copy the database and fix permissions
+echo "Copy DB and fix permissions"
+ssh $BOXUSER.backupbox.se "cp /backups/$BOXUSER/tmp.db /backups/$BOXUSER/index.db && chmod 777 /backups/$BOXUSER/index.db && chown www-data:www-data /backups/$BOXUSER/index.db"
+
+# Third, copy the actual files
+echo "Backup content"
+rsync -avz -e ssh --progress --exclude index.db /HDD/devices root@$BOXUSER.backupbox.se:/backups/$BOXUSER/devices
+
 
 # Restart uwsgi on remote server in order to reload database
 # ssh $BOXUSER.backupbox.se '/etc/init.d/uwsgi restart'
