c68f5cfa994f04b9c8225f27cec3e543988491af
root <root@ubtu>
2014-06-28 21:55:53 +0000

    Fixed file system rights bug

--- system/bin/setup.sh
+++ system/bin/setup.sh
@@ -3,6 +3,6 @@
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SYSTEM_DIR="$(dirname "$BIN_DIR")"
 BACKUP_DIR="$(dirname "$SYSTEM_DIR")/data"
-source "$BACKUP_DIR/local.cfg"
 
-sed -e "s/{username}/$1/g" -e "s/{password}/$2/g" $SYSTEM_DIR/config/local.cfg.sample > /HDD/local.cfg
+sed -e "s/{username}/$1/g" -e "s/{password}/$2/g" $SYSTEM_DIR/config/local.cfg.sample > $BACKUP_DIR/local.cfg
+chown www-data:www-data $BACKUP_DIR/local.cfg
