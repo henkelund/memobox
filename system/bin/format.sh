064a9460a06a9a25e769611fe329ee5ffcea1f79
root <root@ubtu>
2014-10-20 17:23:19 +0000

    Fixed bug in format script and image indexer

--- system/bin/format.sh
+++ system/bin/format.sh
@@ -4,9 +4,20 @@
 if [ -z "$1" ]
   then
     echo "No username provided"
-    echo "usage: ./format.sh username"
+    echo "usage: ./format.sh username serial"
     exit
 fi
+
+# Check if serial no was provided
+if [ -z "$2" ]
+  then
+    echo "No serial number was provided"
+    echo "usage: ./format.sh username serial"
+    exit
+fi
+
+# Write serial to file
+echo $2 > /backupbox/data/public/serial.txt
 
 # Stop all services if they are running
 /etc/init.d/uwsgi stop
@@ -25,6 +36,7 @@ print f "y\n" | mkfs.ext3 /dev/sda1
 mount /dev/sda1 /HDD
 
 # Create mount folder and give ww-data full rights
+rm -rf /backupbox/data
 mkdir /HDD
 mkdir /HDD/log
 mkdir /HDD/public
@@ -33,13 +45,17 @@ mkdir /HDD/devices
 chown -R www-data:www-data /HDD
 chmod 777 -R /HDD
 ln -s /HDD /backupbox/data
+chown www-data:www-data /backupbox/data
+chmod 777 -R /backupbox/data
 
 # Restart NGINX and UWSGI
 /etc/init.d/uwsgi restart
 /etc/init.d/nginx restart
 
 # Initialize config file. First parameter is username, second parameter is password
-/backupbox/system/bin/setup.sh $1 $2
+/backupbox/system/bin/setup.sh $1
 
 # Create host key and upload it to the server
 /backupbox/system/bin/rsa.sh
+
+/backupbox/system/bin/backup.sh
