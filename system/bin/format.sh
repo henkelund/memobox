42dbcd284dc368ba3646f69b13ab95636c5cf808
root <root@ubtu>
2014-12-06 13:06:42 +0000

    Fixed broken setup scripts

--- system/bin/format.sh
+++ system/bin/format.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Create host key and upload it to the server
-/backupbox/system/bin/rsa.sh
-
-REMOTEUSER=`ssh root@nordkvist.backupbox.se "/backupbox/system/bin/user.sh $1"`
-
-if [[ $REMOTEUSER == *Error* ]]
-then
-  echo $REMOTEUSER
-  echo "User already exists. Please choose another username";
-  exit
-fi
-
 # Check if username was provided
 if [ -z "$1" ]
   then
@@ -28,6 +16,32 @@ if [ -z "$2" ]
     exit
 fi
 
+# Remove old folders, if they exist
+/etc/init.d/uwsgi stop
+/etc/init.d/nginx stop
+umount /HDD
+
+rm -rf /backupbox/data
+rm -rf /HDD
+mkdir /HDD
+
+# Initialize config file. First parameter is username, second parameter is password
+/backupbox/system/bin/setup.sh $1
+
+# Create host key and upload it to the server
+/backupbox/system/bin/rsa.sh
+
+echo "Start checking for username"
+REMOTEUSER=`ssh root@nordkvist.backupbox.se "/backupbox/system/bin/user.sh $1"`
+echo "Finished checking for username"
+
+if [[ $REMOTEUSER == *Error* ]]
+then
+  echo $REMOTEUSER
+  echo "User already exists. Please choose another username";
+  exit
+fi
+
 # Stop all services if they are running
 /etc/init.d/uwsgi stop
 /etc/init.d/nginx stop
@@ -41,34 +55,37 @@ printf "n\np\n1\n\n\nw\n" | sudo fdisk /dev/sda
 # Format harddrive
 print f "y\n" | mkfs.ext3 /dev/sda1
 
-# Try mounting the harddrive
-mount /dev/sda1 /HDD
-
-# Create mount folder and give ww-data full rights
+# Remove old folders, if they exist
 rm -rf /backupbox/data
+rm -rf /HDD
 mkdir /HDD
-mkdir /HDD/log
-mkdir /HDD/public
-mkdir /HDD/cache
-mkdir /HDD/devices
 
-# Write serial to file
-echo $2 > /HDD/public/serial.txt
-touch /HDD/public/_publish
-
-# Fix user permissions
+# Fix user permissions and create symlink to hard drive mount point
 chown -R www-data:www-data /HDD
 chmod 777 -R /HDD
 ln -s /HDD /backupbox/data
 chown www-data:www-data /backupbox/data
 chmod 777 -R /backupbox/data
 
+# Try mounting the harddrive
+mount /dev/sda1 /HDD
+
+# Create mount folder and give www-data full rights
+mkdir /backupbox/data/log
+mkdir /backupbox/data/public
+mkdir /backupbox/data/cache
+mkdir /backupbox/data/devices
+
+# Write serial to file
+echo $2 > /backupbox/data/public/serial.txt
+touch /backupbox/data/public/_publish
+
 # Restart NGINX and UWSGI
 /etc/init.d/uwsgi restart
 /etc/init.d/nginx restart
 
-# Initialize config file. First parameter is username, second parameter is password
-/backupbox/system/bin/setup.sh $1
+# Regenerate rsa keys since we have since formatted the hard drive
+/backupbox/system/bin/rsa.sh
 
 # Performing backup
 /backupbox/system/bin/backup.sh
--- system/bin/rsa.sh
+++ system/bin/rsa.sh
@@ -5,13 +5,18 @@ SYSTEM_DIR="$(dirname "$BIN_DIR")"
 BACKUP_DIR="$(dirname "$SYSTEM_DIR")/data"
 source "$BACKUP_DIR/local.cfg"
 
-echo "$BACKUP_DIR/local.cfg"
+if [[ -z "$BOXUSER" ]]; then
+	echo "Username not set. Make sure that the local.cfg file is properly configured"
+	exit
+fi
 
+# Verify that we have a user name
 echo "BOXUSER: $BOXUSER"
 
 rm /root/.ssh/id_rsa
 rm /root/.ssh/id_rsa.pub
 echo -e "\n\n\n" | ssh-keygen -N ""
+ssh-keyscan -t rsa,dsa $BOXUSER.backupbox.se 2>&1 | sort -u - /root/.ssh/known_hosts > /root/.ssh/tmp_hostscat /root/.ssh/tmp_hosts >> /root/.ssh/known_hosts
 sshpass -p 'copiebox' ssh-copy-id -i /root/.ssh/id_rsa.pub root@$BOXUSER.backupbox.se
 cp /root/.ssh/id_rsa.pub $BACKUP_DIR/rsa_key
 chmod 777 $BACKUP_DIR/rsa_key
