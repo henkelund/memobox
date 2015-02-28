858283733fab57b5b25e21fecaae777973c0db06
root <root@ubtu>
2015-02-28 17:55:55 +0000

    Added cpu temperature to ping

--- system/bin/gatherData.sh
+++ system/bin/gatherData.sh
@@ -10,20 +10,30 @@ source "$BACKUP_DIR/local.cfg"
 
 # Load helper functions for getting system data
 source $BIN_DIR/pinghelper.sh
+
 # Load the public IP of the memorybox
 pip=$(curl --connect-timeout 5 -s -X GET http://$BOXUSER.backupbox.se/public_ip)
 echo "Public IP Address: $pip" > $INFOFILE
+
 # Load the local IP on the router
 lip=$(/sbin/ifconfig eth0 | /bin/grep 'inet addr:' | /usr/bin/cut -d: -f2 | /usr/bin/awk '{ print $1}')
 echo "Local IP Address: $lip" >> $INFOFILE
+
 #Load Unique ID of backup hard drive
 uuid=`/sbin/blkid -s UUID -o value /dev/sda1`
 echo "Unique Device ID: $uuid" >> $INFOFILE
+
 # Calculate free and total space of the backup drive
 freespace human $BACKUP_DIR >> $INFOFILE
 
+# Get current commit from Git repo
 ver=$(cd /backupbox && git log -1 --format=%cd .)
 echo "Current Software Version: $ver" >> $INFOFILE
+
+# Get CPU temperature
+temp=`cat /sys/devices/platform/sunxi-i2c.0/i2c-0/0-0034/temp1_input`
+echo "CPU Temperature: $temp" >> $INFOFILE
+
 # Load information of all backed up devices
 #gatherdevices $1>> $INFOFILE
 echo " " >> $INFOFILE
@@ -45,4 +55,5 @@ echo "devicecount=$devicecount&" >> $PINGFILE
 cachecount=`find $BACKUP_DIR/cache -type f | wc -l`
 echo "cachecount=$cachecount&" >> $PINGFILE
 echo "software=$ver&" >> $PINGFILE
+echo "temp=$temp&" >> $PINGFILE
 freespace computer $BACKUP_DIR >> $PINGFILE
