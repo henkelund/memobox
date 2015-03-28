bcf3bb11f32eb27408e76b06ad9a885ac356884e
root <root@ubtu>
2015-03-28 09:27:49 +0000

    tidy up Evenet helper and add support for forced log backup

--- system/lib/python/helper/event.py
+++ system/lib/python/helper/event.py
@@ -7,16 +7,14 @@ import urllib2, json, subprocess
 class EventHelper(object):
     """Helper class for user access management"""
 
-    EVENT_TYPES = [
-    	"SYSTEM",
-   		"INFORMATION", 
-   		"CONFIGURATION", 
-   		"REBOOT", 
-   		"SHUTDOWN"
-        "UPDATE", 
-        "LOGBACKUP",
-        "FULLBACKUP"
-    ]
+    EVENT_SYSTEM        = 0 # Not used at the moment
+    EVENT_INFORMATION   = 1 # Not used at the moment
+    EVENT_CONFIGURATION = 2
+    EVENT_REBOOT        = 3
+    EVENT_SHUTDOWN      = 4 # Not used at the moment
+    EVENT_UPDATE        = 5
+    EVENT_LOGBACKUP     = 6
+    EVENT_FULLBACKUP    = 7 # Not used at the moment
 
     @staticmethod
     def parse_events(data):
@@ -27,7 +25,7 @@ class EventHelper(object):
         if 'messages' in j:
             for message in j["messages"]:
                 message_id = int(message["type"])
-            	if EventHelper.EVENT_TYPES[message_id] == EventHelper.EVENT_TYPES[2]:
+            	if message_id == EventHelper.EVENT_CONFIGURATION:
                     key = message["message"].split("=")[0]
                     value = message["message"].split("=")[1]
 
@@ -38,10 +36,12 @@ class EventHelper(object):
                         c.save()
                     except IndexError as e:
                         print e
-                elif EventHelper.EVENT_TYPES[message_id] == EventHelper.EVENT_TYPES[3]:
+                elif message_id == EventHelper.EVENT_REBOOT:
                     subprocess.call(['/sbin/reboot'])
-                elif EventHelper.EVENT_TYPES[message_id] == EventHelper.EVENT_TYPES[5]:
+                elif message_id == EventHelper.EVENT_UPDATE:
                     subprocess.call(['/backupbox/system/bin/update.sh'])
+                elif message_id == EventHelper.EVENT_LOGBACKUP:
+                    subprocess.call(['/backupbox/system/bin/backup.sh', 'LOGBACKUP'])
 
         return j
 
