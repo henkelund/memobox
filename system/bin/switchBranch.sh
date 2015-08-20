0c3225fcb217ad8e86d4361fe3da251da5dfe137
System Administrator <pm.nordkvist@gmail.com>
2015-08-20 15:16:03 +0200

    Bugfix in switch branch

--- system/bin/switchBranch.sh
+++ system/bin/switchBranch.sh
@@ -4,10 +4,10 @@ mv /backupbox /_backupbox
 git clone https://github.com/lostkeys/backupbox /backupbox
 chown -h www-data:www-data /backupbox/data
 chown -h www-data:www-data /backupbox/system/data
-chmod -h www-data:www-data /backupbox/system/www/static/.webassets-cache
-chmod -h www-data:www-data /backupbox/system/www/static/gen
-chmod -h www-data:www-data /backupbox/system/www/static/devices
-chmod -h www-data:www-data /backupbox/system/www/static/cache
-chmod -h www-data:www-data /backupbox/system/www/static/devices
+chown -h www-data:www-data /backupbox/system/www/static/.webassets-cache
+chown -h www-data:www-data /backupbox/system/www/static/gen
+chown -h www-data:www-data /backupbox/system/www/static/devices
+chown -h www-data:www-data /backupbox/system/www/static/cache
+chown -h www-data:www-data /backupbox/system/www/static/devices
 /backupbox/system/bin/pip-install.sh
 /backupbox/system/bin/update.sh
